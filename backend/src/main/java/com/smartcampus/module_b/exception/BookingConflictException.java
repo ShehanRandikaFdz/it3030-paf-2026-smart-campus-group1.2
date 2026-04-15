ca0c02ffdf389b39854f9ef7f247576e73582cd4
Shashindhie1234 <shashindhieravishka@gmail.com>
2026-04-15 09:30:40 +0530

    implemented the booking management logic and relevent controllers,dtos and entitnies for the backend

---
+++ backend/src/main/java/com/smartcampus/module_b/exception/BookingConflictException.java
@@ -0,0 +1,11 @@
+package com.smartcampus.module_b.exception;
+
+public class BookingConflictException extends RuntimeException {
+    public BookingConflictException(String message) {
+        super(message);
+    }
+
+    public BookingConflictException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
